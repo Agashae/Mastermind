97967c4cbca9048773852b9fa0861a6db01e5217
Agashae <pv31hsu@eduvaud.ch>
2025-07-05 23:22:27 +0200

    code (program.cs) ajout 3 options dans le menu

---
+++ Mastermind/Mastermind/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Mastermind
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mastermind!");
+            Console.WriteLine("Que voulez-vous faire ?");
+            Console.WriteLine();
+            Console.WriteLine("0. Quitter");
+            Console.WriteLine("1. Voir les règles");
+            Console.WriteLine("3. Jouer");
+
+            ConsoleKey key = Console.ReadKey(true).Key;
+
+
+            do
+            {
+                Console.Clear();   
+                Console.WriteLine("Welcome to Mastermind!");
+                Console.WriteLine("Que voulez-vous faire ?");
+                Console.WriteLine();
+                Console.WriteLine("0. Quitter");
+                Console.WriteLine("1. Voir les règles");
+                Console.WriteLine("3. Jouer");
+
+
+                key = Console.ReadKey(true).Key;
+
+                if (key == ConsoleKey.Q)
+                {
+                    Quitter();
+                }
+                else if (key == ConsoleKey.R)
+                {
+                    AfficherRegles();
+                }
+                else if (key == ConsoleKey.J)
+                {
+                    Jouer();
+                }
+            } while (key != ConsoleKey.Q && key != ConsoleKey.R && key != ConsoleKey.J);
+
+
+            Console.ReadLine();
+        }
+
+        static void Quitter()
+        {
+            Environment.Exit(0);
+        }
+
+        static void AfficherRegles()
+        {
+            Console.WriteLine("Règles du jeu Mastermind :");
+        }
+
+        static void Jouer()
+        {
+            Console.Clear();
+           Console.WriteLine("Bienvenue dans le jeu Mastermind !");
+            Console.WriteLine("Dans ce jeu, vous devez deviner une combinaison de couleurs secrètes.");
+
+        }
+    }
+}
