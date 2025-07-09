8cd24fb0a265c3d340789eb456d4851a7baf511a
Agashae <pv31hsu@eduvaud.ch>
2025-07-10 01:11:19 +0200

    code ajout d'un map et d'un exemple

--- Mastermind/Mastermind/Program.cs
+++ Mastermind/Mastermind/Program.cs
@@ -1,30 +1,23 @@
-﻿namespace Mastermind
+﻿using System.Runtime.CompilerServices;
+
+namespace Mastermind
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Welcome to Mastermind!");
             Console.WriteLine("Que voulez-vous faire ?");
             Console.WriteLine();
-            Console.WriteLine("0. Quitter");
-            Console.WriteLine("1. Voir les règles");
-            Console.WriteLine("3. Jouer");
-
+            Console.WriteLine("Q. Quitter");
+            Console.WriteLine("R. Voir les règles");
+            Console.WriteLine("J. Jouer");
             ConsoleKey key = Console.ReadKey(true).Key;
 
 
             do
             {
-                Console.Clear();   
-                Console.WriteLine("Welcome to Mastermind!");
-                Console.WriteLine("Que voulez-vous faire ?");
-                Console.WriteLine();
-                Console.WriteLine("0. Quitter");
-                Console.WriteLine("1. Voir les règles");
-                Console.WriteLine("3. Jouer");
-
-
                 key = Console.ReadKey(true).Key;
 
                 if (key == ConsoleKey.Q)
@@ -52,15 +45,84 @@
 
         static void AfficherRegles()
         {
+            Console.Clear();
             Console.WriteLine("Règles du jeu Mastermind :");
+            Console.WriteLine();
+            Console.WriteLine("Voilà les différentes couleurs");
+            Console.WriteLine();
+
+
+            int MargeHorizontale = 1;
+            int MargeVerticale = 5;
+
+
+            Console.SetCursorPosition(MargeHorizontale, MargeVerticale);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("█ rouge");
+            Console.ResetColor();
+            Console.SetCursorPosition(MargeHorizontale, MargeVerticale + 2);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("█ vert");
+            Console.ResetColor();
+            Console.SetCursorPosition(MargeHorizontale, MargeVerticale + 4);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("█ bleu");
+            Console.ResetColor();
+            Console.SetCursorPosition(MargeHorizontale, MargeVerticale + 6);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("█ jaune");
+            Console.ResetColor();
+            Console.SetCursorPosition(MargeHorizontale, MargeVerticale + 8);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("█ cyan");
+            Console.ResetColor();
+
+            Console.SetCursorPosition(MargeHorizontale + 20, MargeVerticale);
+            Console.WriteLine("Voici un exemple");
+            Map(MargeHorizontale, MargeVerticale);
+
+            Console.SetCursorPosition(MargeHorizontale + 21, MargeVerticale + 2);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("█");
+
+            Console.SetCursorPosition(MargeHorizontale + 23, MargeVerticale + 2);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("█");
+
+            Console.SetCursorPosition(MargeHorizontale + 25, MargeVerticale + 2);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("█");
+
+            Console.SetCursorPosition(MargeHorizontale + 27, MargeVerticale + 2);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("█");
+
+
+
         }
 
         static void Jouer()
         {
             Console.Clear();
-           Console.WriteLine("Bienvenue dans le jeu Mastermind !");
+            Console.WriteLine("Bienvenue dans le jeu Mastermind !");
             Console.WriteLine("Dans ce jeu, vous devez deviner une combinaison de couleurs secrètes.");
 
+
+
+        }
+
+        static void Map(int MargeHorizontale, int MargeVerticale)
+        {
+                string PremiereLigne = "┌─┬─┬─┬─┐";
+                string DeuxiemeLigne = "│ │ │ │ │";
+               string TroisiemeLigne = "└─┴─┴─┴─┘";
+
+            Console.SetCursorPosition(MargeHorizontale + 20, MargeVerticale + 1);
+            Console.WriteLine(PremiereLigne);
+            Console.SetCursorPosition(MargeHorizontale + 20, MargeVerticale + 2);
+            Console.WriteLine(DeuxiemeLigne);
+            Console.SetCursorPosition(MargeHorizontale + 20, MargeVerticale + 3);
+            Console.WriteLine(TroisiemeLigne);
         }
     }
 }
